c0fa66eb3cd4b3eb01b5924de038996b2cd9f060
unknown <abeeralshlawi@gmail.com>
2024-11-13 20:50:57 +0300

    add semantic versioning

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Post, Body } from '@nestjs/common';
 import { ArticleService } from './article.service';
 import { CreateArticleDto } from './dto/create-article.dto';
 
-@Controller('/articles')
+@Controller({ path: 'articles', version: '1.0' })
 export class ArticleController {
   constructor(private readonly articleService: ArticleService) {}
 
