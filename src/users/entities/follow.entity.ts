405fcc70cdc84ffb0299270cdfa42f825a44aa59
unknown <abeeralshlawi@gmail.com>
2024-11-29 14:36:12 +0300

    fix indexing bug

--- src/users/entities/follow.entity.ts
+++ src/users/entities/follow.entity.ts
@@ -10,7 +10,7 @@ export class Follow {
   @ManyToOne(() => User, (user) => user.followers, { onDelete: 'CASCADE' })
   follower: User;
 
-  @Index('idx_follow_on_follower_id')
+  @Index('idx_follow_on_following_id')
   @ManyToOne(() => User, (user) => user.followings, { onDelete: 'CASCADE' })
   following: User;
 }
